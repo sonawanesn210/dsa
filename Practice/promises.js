f335f99feea311a214d583b1c08f80ae635e916b
Swapnali <sonawanesn210@gmail.com>
2022-11-07 14:56:26 +0530

    added promise

--- Practice/promises.js
+++ Practice/promises.js
@@ -15,3 +15,30 @@ const promise = new Promise(function (resolve, reject) {
     .catch(function () {
       console.log("Promise is rejected");
     });
+
+
+    /////////using set time out
+
+    const promise1 = new Promise(function (resolve, reject) {
+      const string1 = "geeksforgeeks";
+      const string2 = "geeksforgeeks";
+      if (string1 === string2) {
+        setTimeout(()=>{
+          resolve("Promise resolved successfully");
+        },2000)
+       
+      } else {
+        setTimeout(()=>{
+          reject("Promise is rejected");
+        },2000)
+       
+      }
+    });
+   
+    promise1
+      .then(function (data) {
+        console.log(data);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
