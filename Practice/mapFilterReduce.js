74dea859f560f90aa50d9afc457c5944e43c3355
Swapnali-zvolv <Swapnali@zvolv.com>
2023-08-01 14:40:51 +0530

    added map examples

--- Practice/mapFilterReduce.js
+++ Practice/mapFilterReduce.js
@@ -18,3 +18,16 @@ let output3= arr.reduce((acc,cur)=>{
     return acc
 },0)
 console.log(output3)
+
+
+const numbers = [1, 2, 3, 4, 5];
+const squaredNumbers = numbers.map((num) => num * num);
+console.log(squaredNumbers); // Output: [1, 4, 9, 16, 25]
+
+const names = ["john", "mary", "alice"];
+const uppercaseNames = names.map((name) => name.toUpperCase());
+console.log(uppercaseNames); // Output: ["JOHN", "MARY", "ALICE"]
+
+const fruits = ["apple", "banana", "orange"];
+const lengths = fruits.map((fruit) => fruit.length);
+console.log(lengths); // Output: [5, 6, 6]
